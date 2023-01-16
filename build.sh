b5fb3779e9a022a2366f83932a0466af9ec27691
Shaft <sh4ft.me@gmail.com>
2023-01-16 09:48:22 +0100

    Intitial project

---
+++ build.sh
@@ -0,0 +1,4 @@
+# shellcheck disable=SC2035
+# shellcheck disable=SC2164
+cd src/ShootEmUp
+gcc -Wall *.c -lm -lSDL2 -lSDL2_image -lSDL2_mixer
---
+++ run.sh
@@ -0,0 +1,3 @@
+# shellcheck disable=SC2164
+cd src/ShootEmUp
+./a.out
